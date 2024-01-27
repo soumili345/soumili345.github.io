c48c89e1890f388d1bdde2af99454f748071a1cd
soumili345 <80914204+soumili345@users.noreply.github.com>
2024-01-26 21:22:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/headshot.jpg") {
+    myImage.setAttribute("src", "images/king-julian.jpg");
+  } else {
+    myImage.setAttribute("src", "images/headshot.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h2");
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to my website, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome to my website, ${storedName}`;
+}
+
